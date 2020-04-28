e2035c3e630438d923b3487d026026b618d38e4c
Luis Carbajal <luis.carbajal@utec.edu.pe>
2020-04-27 20:18:08 -0500

    Project created

---
+++ Tarea 3/build/CMakeFiles/Tarea_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_3.dir/src/main.cpp.obj"
+  "Tarea_3.exe"
+  "Tarea_3.exe.manifest"
+  "Tarea_3.pdb"
+  "libTarea_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
